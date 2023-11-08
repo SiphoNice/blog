e07ab6fee189ae6eebac2280444eaa9dfd18cb5c
Derrick <houseinthehills2020@gmail.com>
2023-11-08 10:39:02 +0200

    feat: new files

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,5 +1,37 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 export default function Home() {
-  return <p>Home page...</p>;
+  const [posts, setPosts] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:3001/posts").then((response) => {
+      setPosts(response.data);
+    });
+  }, []);
+  return (
+    <>
+      <div className="container">
+        {posts.map((post, key) => (
+          <div className="row" key={post.id}>
+            <div className="col-lg-8 col-md-10 mx-auto">
+              <div className="post-heading">
+                <h2 className="subheading">{post.title}</h2>
+                <span className="meta">
+                  Posted by <Link to="/" className="text-danger">{post.username}</Link>
+                </span>
+                <p>
+                  <small className="text-danger">{post.createdAt}</small>
+                </p>
+              </div>
+            </div>
+            <div className="col-lg-8 col-md-10 mx-auto">
+              {post.postDescription}
+              <hr />
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require("express"); 
 const app = express();
-app.use(express.json());
-const db = require('./models');
+const cors = require("cors");
 
+app.use(express.json());
+app.use(cors());
+
+const db = require('./models');
 // using Router as a middleware
 const postRouter = require('./routes/posts');
 app.use("/posts", postRouter);
-
 //By doing this you checking if all tables created on the models exits, if not create the table on the database
 db.sequelize.sync().then(()=>{
     app.listen(3001, () => {
